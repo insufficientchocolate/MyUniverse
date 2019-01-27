7df19ce2f814266f3f3df73dc1bdaa6fa1795f72
insufficientchocolate <tony84727@gmail.com>
2019-01-27 16:56:04 +0800

    make screen green

--- window_manager.cc
+++ window_manager.cc
@@ -1,25 +1,35 @@
 #include "window_manager.hpp"
 #include <GL/glew.h>
 #include <OpenGL/gl.h>
+#include <chrono>
 #include <stdexcept>
 #include "error.hpp"
 #include "sdl_error.hpp"
 
 namespace Universe::Graphic {
-WindowManager::WindowManager(SDL_Window* window) {
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, true);
-  SDL_GLContext gContext = SDL_GL_CreateContext(window);
-  if (gContext == nullptr) {
-    throw Universe::Errors::SDLError{"OpenGL"};
-  }
-  SDL_GL_SetSwapInterval(1);
-  glewExperimental = GL_TRUE;
-  if (glewInit() != GLEW_OK) {
-    throw Universe::Errors::Error{"GLEW"};
-  }
-
-  glClearColor(0, 0, 1, 1);
-  glClear(GL_COLOR_BUFFER_BIT);
-  SDL_GL_SwapWindow(window);
+WindowManager::WindowManager(SDL_Window* window)
+    : window_(window), running_(true) {
+  startRenderingThread();
 }
-};  // namespace Universe::Graphic
+void WindowManager::renderingLoop() { glClear(GL_COLOR_BUFFER_BIT); }
+void WindowManager::startRenderingThread() {
+  std::thread t([&] {
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, true);
+    SDL_GLContext gContext = SDL_GL_CreateContext(window_);
+    if (gContext == nullptr) {
+      throw Universe::Errors::SDLError{"OpenGL"};
+    }
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+      throw Universe::Errors::Error{"GLEW"};
+    }
+    glClearColor(0, 1, 0, 1);
+    while (running_) {
+      renderingLoop();
+      SDL_GL_SwapWindow(window_);
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+  });
+  t.detach();
+}
+}  // namespace Universe::Graphic
--- window_manager.hpp
+++ window_manager.hpp
@@ -1,10 +1,18 @@
 #ifndef _UNIVERSE_WINDOW_MANAGER_H_
 #define _UNIVERSE_WINDOW_MANAGER_H_
 #include <SDL.h>
+#include <memory>
+#include <thread>
 namespace Universe::Graphic {
 class WindowManager {
  public:
   WindowManager(SDL_Window* window);
+
+ private:
+  bool running_;
+  void startRenderingThread();
+  void renderingLoop();
+  SDL_Window* window_;
 };
 };  // namespace Universe::Graphic
 #endif
