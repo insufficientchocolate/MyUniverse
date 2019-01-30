f9f3329a1bebea9bba7127a04b272a75f758ab9f
insufficientchocolate <tony84727@gmail.com>
2019-01-30 21:47:57 +0800

    add gl_program_manager

--- gl_program_manager.cc
+++ gl_program_manager.cc
@@ -1,37 +1,58 @@
 #include "gl_program_manager.hpp"
+#include <fstream>
+#include <sstream>
 #include <vector>
+#include "error.hpp"
 
-namespace Universe
-{
-  GLProgramManager::GLProgramManager(const std::string& shaderDirectory): sourceDir_(shaderDirectory)
-  {}
+namespace Universe {
+GLProgramManager::GLProgramManager(const std::string& shaderDirectory)
+    : sourceDir_(shaderDirectory) {}
 
-  static GLuint compileShader(char const ** source,GLenum shaderType) {
-	GLuint shader = glCreateShader(shaderType);
-	glShaderSource(shader, 1, source, nullptr);
-	glCompileShader(shader);
-	GLint result;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
-	if (result != GL_TRUE) {
-	  GLint logLength;
-	  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
-	  std::vector<char> errorMessage(logLength + 1);
-	  glGetShaderInfoLog(shader, logLength,nullptr, &errorMessage[0]);
-	  throw Error("OpenGL", errorMessage);
-	}
+static GLuint compileShader(const std::string& source, GLenum shaderType) {
+  GLuint shader = glCreateShader(shaderType);
+  char const* sourceChar = source.c_str();
+  glShaderSource(shader, 1, &sourceChar, nullptr);
+  glCompileShader(shader);
+  GLint result;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+  if (result != GL_TRUE) {
+    GLint logLength;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<char> errorMessage(logLength + 1);
+    glGetShaderInfoLog(shader, logLength, nullptr, &errorMessage[0]);
+    std::string error(errorMessage.begin(), errorMessage.end());
+    throw Errors::Error("OpenGL", error);
   }
-
-  GLuint GLProgramManager::compileProgram(const std::string& vertexPath,const std::string& fragmentPath)
-  {
-	std::string sources = loadSource(vertexPath);
-	char const *vectorSourcePointer = sources.c_str();
-	glShaderSource(vShader, 1,&vectorSourcePointer, NULL)
-	glCompileShader(vShader);
-	glGetShaderiv(vShader, GL_COMPILE_STATUS, &result);
-
-  }
-
-  const std::string loadSource(const std::string& path)
-  {
-  }
+  return shader;
 }
+
+GLuint GLProgramManager::compileProgram(const std::string& vertexPath,
+                                        const std::string& fragmentPath) const {
+  GLuint vShader = compileShader(loadSource(vertexPath), GL_VERTEX_SHADER);
+  GLuint fShader = compileShader(loadSource(fragmentPath), GL_FRAGMENT_SHADER);
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vShader);
+  glAttachShader(program, fShader);
+  glLinkProgram(program);
+
+  GLint result;
+  glGetProgramiv(program, GL_LINK_STATUS, &result);
+  if (result != GL_TRUE) {
+    GLint logLength;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<char> errorMessage(logLength + 1);
+    glGetProgramInfoLog(program, logLength, nullptr, &errorMessage[0]);
+    std::string error(errorMessage.begin(), errorMessage.end());
+    throw Errors::Error("OpenGL", error);
+  }
+  return program;
+}
+
+const std::string GLProgramManager::loadSource(const std::string& path) const {
+  std::fstream fin((sourceDir_ / path).native());
+  fin.exceptions(std::fstream::badbit);
+  std::stringstream ss;
+  ss << fin.rdbuf();
+  return ss.str();
+}
+}  // namespace Universe
--- gl_program_manager.hpp
+++ gl_program_manager.hpp
@@ -1,17 +1,20 @@
 #ifndef _UNIVERSE_GL_PROGRAM_MANAGER_HPP
-#define _UNIVERSE_GL_PROGRAM_MANAGER_HPP 
+#define _UNIVERSE_GL_PROGRAM_MANAGER_HPP
 #include <GL/glew.h>
 #include <SDL.h>
+#include <boost/filesystem.hpp>
+#include <string>
 
-namespace Universe
-{
-  class GLProgramManager
-  {
-	public:
-	  GLuint compileProgram(const std::string& vertexPath,const std::string& fragmentPath);
-	private:
-	  const std::string sourceDir_;
-	  const std::string loadSource(const std::string& path);
-  }
+namespace Universe {
+class GLProgramManager {
+ public:
+  GLProgramManager(const std::string& shaderDirectory);
+  GLuint compileProgram(const std::string& vertexPath,
+                        const std::string& fragmentPath) const;
+
+ private:
+  const boost::filesystem::path sourceDir_;
+  const std::string loadSource(const std::string& path) const;
 };
+};     // namespace Universe
 #endif /* GL_PROGRAM_MANAGER_HPP */
